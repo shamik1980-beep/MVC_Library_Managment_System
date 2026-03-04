828f9b9545e8016967d250afa96a5738ea10faf2
Shamik Palit <shamik1980@gmail.com>
2026-03-04 10:13:34 +0400

    Create Controller

---
+++ BookController.java
@@ -0,0 +1,28 @@
+package com.lab.mvc.controller;
+
+import com.lab.mvc.model.BookRepository;
+import com.lab.mvc.view.BookView;
+
+public class BookController {
+    private final BookRepository repo;
+    private final BookView view;
+
+    public BookController(BookRepository repo, BookView view) {
+        this.repo = repo;
+        this.view = view;
+    }
+
+    public void start() {
+        boolean run = true;
+        while (run) {
+            view.showMenuMain();
+            int choice = view.readChoice();
+
+            switch (choice) {
+                case 1 -> view.showBooks(repo.findAll());
+                case 2 -> { view.message("Bye!"); run = false; }
+                default -> view.message("Choose 1 or 2 only.");
+            }
+        }
+    }
+}
