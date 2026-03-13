e6101c3bdc5e8d637e03be8bffe0f7ff1b73f03e
Shamik Palit <shamik1980@gmail.com>
2026-03-13 11:19:14 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import com.lab.mvc.controller.BookController;
 import com.lab.mvc.model.BookRepository;
 import com.lab.mvc.view.BookView;
 
+// This is the entry point of the application 
+
 public class Main {
     public static void main(String[] args) {
         BookRepository repo = new BookRepository();
